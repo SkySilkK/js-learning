583041e1dfa074e2d47bb3f00883362aa57bee3c
SkySilk <79651976+SkySilkK@users.noreply.github.com>
2024-04-18 10:10:31 +0000

    Datatypes

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,14 @@
-console.log("Hello")
+const personName="Raj"
+let personAge=22
+let personEmploymentStatus
+let la=null
+
+personAge=23
+
+/* Do not use var for initializing variable cause problems in global and local scoping use let instead */
+
+
+console.log(personName, personAge, personEmploymentStatus,null)
+
+console.table([personName, personAge, personEmploymentStatus,null])
+console.log(typeof la)
